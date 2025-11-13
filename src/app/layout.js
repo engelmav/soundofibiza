ac1ca59929813206cd93b461d13d5d1f54b2ed2f
Vincent Engelmann <vincent.engelmann1@gmail.com>
2025-11-13 12:52:41 -0500

    Add Google Analytics tracking code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Rent Party Speakers',
@@ -8,6 +9,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T3Z6H11NJ6"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T3Z6H11NJ6');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );

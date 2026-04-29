c904560a2cb335e47b9c61206d86d03f97272644
Vincent Engelmann <vincent.engelmann1@gmail.com>
2026-04-29 14:32:24 -0400

    Use beforeFiles for subdomain rewrite per Vercel docs

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'balloonpalace.soundofibiza.com' }],
-        destination: '/balloon-palace/:path*',
-      },
-    ];
+    return {
+      beforeFiles: [
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: 'balloonpalace.soundofibiza.com' }],
+          destination: '/balloon-palace/:path*',
+        },
+      ],
+    };
   },
 };
 
